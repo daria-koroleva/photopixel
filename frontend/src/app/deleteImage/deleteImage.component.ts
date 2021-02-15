c3462c7510fa08b9f7d0fff66cd9770c2b65f941
chemoautotroph <niekaiyu990206@icloud.com>
2021-02-15 14:41:19 -0500

    #18 upload image modify branch with upload image function
    #18 upload image function should be working but delete image is still in progress.

---
+++ frontend/src/app/deleteImage/deleteImage.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'delete-Image',
+  templateUrl: './deleteImage.component.html',
+  styleUrls:['./deleteImage.component.css']
+})
+export class DeleteImageComponent {
+  constructor() { }
+}
