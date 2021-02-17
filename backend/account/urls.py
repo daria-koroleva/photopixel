17f04697334369ff6f8f869094cd7a13825d17e5
woshidiyi1228 <62082095+woshidiyi1228@users.noreply.github.com>
2021-02-17 12:19:42 +0800

    #38 Add url pattern to the follow view.

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('profile/', views.ProfileView.as_view()),
     path('api/auth/login/', views.CustomAuthToken.as_view()),
     path('api/auth/register/', views.AccountCreateAPIView.as_view()),
+    path('api/auth/follow/', views.FollowView.as_view()),
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
