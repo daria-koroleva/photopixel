13fc303b7614c56ac04f4c6d779687d8212034af
rsleyland <71075940+rsleyland@users.noreply.github.com>
2021-02-18 08:25:35 -0500

    Changed to PostRetrieveUpdateDestroy
    from PostRetrieveDestroy

--- backend/post/urls.py
+++ backend/post/urls.py
@@ -7,7 +7,7 @@ from backend.post import views
 urlpatterns = [
     path('post/saveFile/', views.saveFile),
     path('post/', views.PostList.as_view()),
-    path('post/<int:pk>', views.PostRetrieveDestroy.as_view()),
+    path('post/<int:pk>', views.PostRetrieveUpdateDestroy.as_view()),
     path('post/<int:pk>/like', views.LikeCreate.as_view()),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns = format_suffix_patterns(urlpatterns)
