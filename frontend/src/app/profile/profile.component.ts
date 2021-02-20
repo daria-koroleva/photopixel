66be2595b8c2b66e9f24fc878dab5cb90bd70ce8
chemoautotroph <niekaiyu990206@icloud.com>
2021-02-20 14:18:12 -0500

    #44 upload detele method, not sure its working tho

--- frontend/src/app/profile/profile.component.ts
+++ frontend/src/app/profile/profile.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from './../api.service';
 import { first, map } from 'rxjs/operators'
+//import { deleteImage } from './../deleteImage/deleteImage.component';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { formatCurrency } from '@angular/common';
 
 @Component({
   selector: 'app-profile',
@@ -11,8 +14,8 @@ export class ProfileComponent implements OnInit {
 
   posts: any;
   currentUserName: string;
-  constructor(private api : ApiService) { 
-   
+  constructor(private api : ApiService, private http: HttpClient) {
+
   }
 
   ngOnInit(): void {
@@ -37,6 +40,24 @@ export class ProfileComponent implements OnInit {
         this.posts = post;
       }
     );
+  }
+
+  deleteImage(post:any){
+    let post_id = post.id;
+    console.log("delete image button is pressed");
+    console.log(post_id);
+    //const fd = new FormData();
+    //fd.append('image',this.posts.id);
+    this.http.delete("http://127.0.0.1:8000/posts/post/"+post_id)
+    .subscribe(val=>{
+      console.log("DELETE call successful value returned in body", val);
+    },
+    res=>{
+      console.log("DELETE call in error", res);
+    },
+    ()=>{
+      console.log("The DELETE is now complete")
+    });
   }
 
 
