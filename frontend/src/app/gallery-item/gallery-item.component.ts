335108d238f5bc7bfff1b40de4134fc7ed269614
chemoautotroph <niekaiyu990206@icloud.com>
2021-03-04 01:01:28 -0500

    #88 Fix bug delete buttom
    Fix a bug where the delete button does not display

--- frontend/src/app/gallery-item/gallery-item.component.ts
+++ frontend/src/app/gallery-item/gallery-item.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Options } from '@flywine93/ngx-imgclicker';
+import { Router } from '@angular/router';
+import { ApiService } from './../api.service';
+import { first, map } from 'rxjs/operators'
+
 
 @Component({
   selector: 'app-gallery-item',
@@ -8,14 +12,18 @@ import { Options } from '@flywine93/ngx-imgclicker';
 })
 export class GalleryItemComponent implements OnInit {
 
+  posts: any;
+  profileId: number=null;
+  profileInfoLoaded :boolean = false;
+  postsLoaded :boolean = false;
 
   @Input() fileName: string;
   @Input() post: any;
 
-  constructor() { }
+  constructor(private api:ApiService, private _router:Router) { }
 
   ngOnInit(): void {
-    
+
   }
 
   title = 'imgclicker-project';
@@ -24,5 +32,27 @@ export class GalleryItemComponent implements OnInit {
       return url;
     }
   };
+
+  getProfilePosts(){
+    this.api.getAllPostsByUserId(this.profileId).pipe(first()).subscribe(
+      post => {
+        this.posts = post;
+        this.postsLoaded = true;
+      }
+    );
+  }
+
+  deleteImage(post:any){
+    let post_id = post.id;
+    console.log("delete image button is pressed");
+    console.log(post_id);
+    this.api.deleteImage(post_id).subscribe(
+      data => {
+        console.log(data);
+        this.posts = null;
+        this._router.navigateByUrl("");
+      }
+    );
+  }
 
 }
