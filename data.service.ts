93fda1979fe404b6fc18203240e4304b64483757
chemoautotroph <niekaiyu990206@icloud.com>
2021-03-05 18:04:42 -0500

    #83 #84 #89 small update
    merge this with the main branch so I can have access with the comment API.

---
+++ data.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
+
+@Injectable()
+export class DataService{
+
+  private messageSource = new BehaviorSubject<string>("Change this latter");
+  currentMessage = this.messageSource.asObservable();
+
+  constructor(){  }
+
+  changeMessage(message: string) {
+    this.messageSource.next(message);
+  }
+
+}
