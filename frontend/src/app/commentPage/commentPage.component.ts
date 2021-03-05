dd8bb6622539ceb5a027337c0fc618096e95fbb7
chemoautotroph <niekaiyu990206@icloud.com>
2021-03-04 19:42:49 -0500

    #83 #84 #89 small update
    add a comment button that can direct the user to a new page which should display all the detail of the pic, comment, likes, @someone, etc. 
    There is a small bug where you have to press the button from the home page rather than the profile page. Since the home page displays the same thing as the profile page, they both have the button. Check the `app-routing.moudule.ts` file in order to fix this.

---
+++ frontend/src/app/commentPage/commentPage.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector:'app-commentPage',
+  templateUrl:'./commentPage.component.html',
+  styleUrls:['./commentPage.component.css']
+})
+export class CommentComponent {
+
+  constructor(){
+
+  }
+}
