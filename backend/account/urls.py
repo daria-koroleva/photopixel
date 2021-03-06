52fcd382cddd8f5c37ac623b0fd19ca53f5f225c
rsleyland <71075940+rsleyland@users.noreply.github.com>
2021-03-06 12:48:24 -0500

    #93 urls to return lists of followers per user id
    and return list of users the passed user id is following

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('users/', views.UserList.as_view()),
     path('api/auth/login/', views.CustomAuthToken.as_view()),
     path('api/auth/register/', views.AccountCreateAPIView.as_view()),
-    path('api/auth/follow/', views.FollowView.as_view()),
+    path('api/auth/follow/', views.FollowView.as_view()), #list of users that logged in user is following
+    path('followersUser/<int:pk>', views.FollowersUserIDList.as_view()), #list of users following this user id
+    path('followingUser/<int:pk>', views.FollowingUserIDList.as_view()), #list of users that this user id follows
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
