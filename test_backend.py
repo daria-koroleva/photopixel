0a11f5c3c4561900440dfa2f71719619fac37a47
zh220 <71472712+zh220@users.noreply.github.com>
2021-03-16 19:38:42 -0400

    #100 #101 add post create/delete unit tests

--- test_backend.py
+++ test_backend.py
@@ -38,3 +38,51 @@ class LoginTestCase(APITestCase):
         }
         response = self.client.post("/accounts/api/auth/login/", data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+        
+        
+class PostCreateTestCase(APITestCase):
+
+    def setUp(self):
+        self.user = Account.objects.create_user(username ="testcase",
+            email = "test@localhost.app",
+            password = "StrongPassword321",
+            profilePhotoFileName = "item1.jpg")
+        self.token = Token.objects.create(user=self.user)
+        self.api_authentication()
+
+    def api_authentication(self):
+        self.client.credentials(HTTP_AUTHORIZATION="Token "+self.token.key)
+
+    def test_postcreate(self):
+        post = {
+            "title" : "Test Title", "content" : "Test content", "photoFileName" : "testFilename.jpg"
+        }
+        response = self.client.post("/posts/post/", post, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+class PostDeleteTestCase(APITestCase):
+
+    def setUp(self):
+        self.user = Account.objects.create_user(username ="testcase",
+            email = "test@localhost.app",
+            password = "StrongPassword321",
+            profilePhotoFileName = "item1.jpg")
+        self.token = Token.objects.create(user=self.user)
+        self.api_authentication()
+        self.createPost()
+        
+    def api_authentication(self):
+        self.client.credentials(HTTP_AUTHORIZATION="Token "+self.token.key)
+
+    def createPost(self):
+        post = {
+            "title" : "Test Title", "content" : "Test content", "photoFileName" : "testFilename.jpg"
+        }
+        return Post.objects.create(title = "Test Title", content = "Test content", photoFileName = "testFilename.jpg", poster=self.user)
+
+    def test_postdelete(self):
+        postId = '1'
+        response = self.client.delete("/posts/post/"+ postId)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
